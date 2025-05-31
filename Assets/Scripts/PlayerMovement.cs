87220c327bca7dd17f44d17fad931121f493165d
TheDLCrimson <duclnc2004@gmail.com>
2025-05-31 18:20:39 +0700

    [FEAT] Added Jump and fix some mesh shit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float groundCheckDistance = 1f;
+
+    public LayerMask whatIsGround;
     private Rigidbody rb;
+
+    private bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,14 +17,27 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, whatIsGround);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * 10f, ForceMode.Impulse);
+        }
+
         Vector3 moveDirection = new Vector3(h, 0f, v).normalized;
         Vector3 moveVelocity = moveDirection * moveSpeed;
 
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + moveVelocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, Vector3.down * groundCheckDistance);
     }
 }
